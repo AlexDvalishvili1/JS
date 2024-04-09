0e5ad281cb8b94ad3b2962ce564ed8be6d02e744
AlexDvalishvili1 <alex-dvalishvili@mail.ru>
2024-04-09 10:45:51 +0200

    Changed Exercise3

--- functions/exercise_3.js
+++ functions/exercise_3.js
@@ -1,15 +1,18 @@
 //Short version
-const getMax = (num1, num2, num3, num4) => Math.max(num1, num2, num3, num4);
+const getMax = (...args) => Math.max(...args);
 
-console.log(getMax(1, 4, 2, 3))
+console.log(getMax(11, 4, 8));
 
 //Long version (for Markus)
-// function getMax(num1, num2, num3, num4) {
-//     let max = num1;
-//     max = num2 > max ? num2 : max;
-//     max = num3 > max ? num3 : max;
-//     max = num4 > max ? num4 : max;
+// function getMax(...args) {
+//     let max = -1;
+//     if (args.length !== 0) {
+//         max = args[0];
+//         for (let num of args) {
+//             max = num > max ? num : max;
+//         }
+//     }
 //     return max; // or I could log immediately
 // }
 //
-// console.log(getMax(1, 4, 2, 3));
+// console.log(getMax(11, 4, 2));
