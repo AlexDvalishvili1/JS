a59dcd1a4a206f7d3dcd8ca89ce0580091f5eae3
AlexDvalishvili1 <alex-dvalishvili@mail.ru>
2024-04-09 10:31:48 +0200

    Fixed Exercise5

--- Objects-Arrays/5_exercise.js
+++ Objects-Arrays/5_exercise.js
@@ -3,16 +3,29 @@ const rl = readline.createInterface({input: process.stdin, output: process.stdou
 const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
 rl.on('close', () => process.exit(0));
 
-const people = {
-    "hans": ["Hans", "Muller", 22],
-    "georg": ["Georg", "Huber", 37],
-    "fritz": ["Fritz", "Mayr", 19],
-};
+const people = [
+    {fname: "Hans", lname: "Muller", age: 22},
+    {fname: "Georg", lname: "Huber", age: 37},
+    {fname: "Fritz", lname: "Mayr", age: 19},
+];
+
+const columnNames = {
+    "hans": 0,
+    "georg": 1,
+    "fritz": 2,
+}
+
+const rowKeys = {
+    1: 'fname',
+    2: 'lname',
+    3: 'age',
+}
 
 async function execute() {
-    const columnName = await prompt("Please enter column name (Hans, Georg, Fritz): ");
+    console.table(people);
+    const column = await prompt("Please enter column name (Hans, Georg, Fritz): ");
     const row = await prompt("Please enter row number (1 - 3): ");
-    console.log(people[columnName.toLowerCase()][row - 1]);
+    console.log(people[columnNames[column.toLowerCase()]][rowKeys[row]]);
 }
 
 execute().catch((err) => {
