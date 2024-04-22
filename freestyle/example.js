0068aba8ca4fae19af8caf9f2e8edf79d5ea19a1
AlexDvalishvili1 <alex-dvalishvili@mail.ru>
2024-04-22 12:01:47 +0200

    Added express.json

--- freestyle/example.js
+++ freestyle/example.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 app.get('/', function (req, res) {
     res.send('Hello World')
+})
+
+app.post('/task', function (req, res) {
+    console.log('Unser backend wurde agerufen mit route /task ', req.body)
+    res.send('You called /users')
 })
 
 app.listen(3000, () => {
